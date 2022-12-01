ef54ff2c2bbe2ffe9f949346deb55dc24b2307c2
ryaaad <devil.fine116@gmail.com>
2022-12-01 22:13:50 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Board from "./CMP/Board";
+
+function App() {
+  return (
+    <div className="grid h-[100vh] justify-center items-center">
+<Board></Board>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/CMP/Board.tsx
@@ -0,0 +1,51 @@
+const Board = () => {
+    return ( 
+        <div  className="grid rounded-md grid-cols-[repeat(7,65px)] p-5 grid-rows-[repeat(6,65px)] gap-3
+       bg-black">
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+      <div className="bg-white rounded-full"></div>
+        </div>
+     );
+}
+ 
+export default Board;
